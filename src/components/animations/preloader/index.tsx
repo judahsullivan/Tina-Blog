7b89112f6fed17780a6530c79475a8499a0b922a
Judah Sullivan <106212053+judahsullivan@users.noreply.github.com>
2023-11-12 16:56:22 -0600

    Update index.tsx
    Working on the video on mobile

--- src/components/animations/preloader/index.tsx
+++ src/components/animations/preloader/index.tsx
@@ -34,13 +34,9 @@ export default function Preloader({ onLoadingComplete }: PreloaderProps) {
   return (
     <div className=" overlay flex text-white font-basement justify-center overflow-hidden items-center bg-black absolute top-0 bottom-0 left-0 right-0 w-full min-h-screen  z-30 ">
       <div className=" items-center justify-center max-w-6xl mx-auto flex flex-col">
-        <video className=" memoji w-[200px] h-[200px] md:w-[400px] md:h-[400px]" 
-  autoPlay={true} 
-    controls={false} 
-    playsInline
-    muted>
-          <source type="video/mp4" src={'/memojiloader.mp4'} />
-        </video>
+      <video className="memoji w-[200px] h-[200px] md:w-[400px] md:h-[400px]" autoPlay muted playsInline controls={false}>
+  <source type="video/mp4" src="/memojiloader.mp4" />
+</video>
         <div className="absolute bottom-[350px] px-2 md:px-16 left-0  md:bottom-[250px] flex text-2xl  justify-between w-full items-center">
           <div className=" overflow-hidden text-3xl md:text-6xl">
             <div className="heading inline-block  ">Passion</div>
